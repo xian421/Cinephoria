df16d18885dadf37d2ad528eae81de4fa51c0f7a
Christian Zweigert <christian.zweigert@krugundpartner.com>
2024-11-25 20:00:28 +0100

    Add frontend files

--- cinephoria-frontend/src/App.svelte
+++ cinephoria-frontend/src/App.svelte
@@ -1,47 +1,28 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  let movies = [];
+
+  async function fetchMovies() {
+      const response = await fetch('https://cinephoria-backend-c53f94f0a255.herokuapp.com/movies');
+      movies = await response.json();
+  }
+
+  fetchMovies();
 </script>
 
-<main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  ul {
+      list-style: none;
+      padding: 0;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+
+  li {
+      margin: 10px 0;
   }
 </style>
+
+<h1>Filmliste</h1>
+<ul>
+  {#each movies as movie}
+      <li>{movie.title}</li>
+  {/each}
+</ul>
