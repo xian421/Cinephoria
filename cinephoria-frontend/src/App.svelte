47c3f532564dea4a495bc0544f4d7d43f47a0705
Christian Zweigert <christian.zweigert@krugundpartner.com>
2024-11-25 21:11:26 +0100

    API frontend vergessen

--- cinephoria-frontend/src/App.svelte
+++ cinephoria-frontend/src/App.svelte
@@ -1,28 +1,17 @@
 <script>
+  import { onMount } from 'svelte';
+
   let movies = [];
 
-  async function fetchMovies() {
-      const response = await fetch('https://cinephoria-backend-c53f94f0a255.herokuapp.com/movies');
-      movies = await response.json();
-  }
-
-  fetchMovies();
+  onMount(async () => {
+    const response = await fetch('https://cinephoria-backend-c53f94f0a255.herokuapp.com/movies');
+    movies = await response.json();
+  });
 </script>
 
-<style> 
-  ul {
-      list-style: none;
-      padding: 0;
-  }
-
-  li {
-      margin: 10px 0;
-  }
-</style>
-
-<h1>Filmliste</h1>
+<h1>Aktuelle Filme</h1>
 <ul>
   {#each movies as movie}
-      <li>{movie.title}</li>
+    <li>{movie.title} - {movie.release_date}</li>
   {/each}
 </ul>
