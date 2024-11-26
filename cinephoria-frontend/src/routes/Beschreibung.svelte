e6e629d9410f9f0ff551dfaae13088a3abb21a2d
Christian Zweigert <christian.zweigert@krugundpartner.com>
2024-11-26 17:10:51 +0100

    Beschreibung

---
+++ cinephoria-frontend/src/routes/Beschreibung.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let movie = {};
+  </script>
+  
+  <h1>{movie.title}</h1>
+  <p>{movie.overview}</p>
+  <p><strong>Release-Datum:</strong> {movie.release_date}</p>
+  <p><strong>Beliebtheit:</strong> {movie.popularity}</p>
+  
+  <style>
+    h1 {
+      font-size: 2rem;
+      color: #333;
+    }
+    p {
+      font-size: 1rem;
+      color: #555;
+    }
+  </style>
+  
