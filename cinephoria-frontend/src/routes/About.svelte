50dbafbd7cdd2dd699f44c83e52c8232794f3075
Christian Zweigert <christian.zweigert@krugundpartner.com>
2024-11-26 12:35:31 +0100

    Fix About page route

--- cinephoria-frontend/src/routes/About.svelte
+++ cinephoria-frontend/src/routes/About.svelte
@@ -1,2 +1,13 @@
-<h1>About</h1>
-<p>This is the About page.</p>
+<script>
+    let name = 'About Page';
+  </script>
+  
+  <h1>{name}</h1>
+  <p>Welcome to the About page of our Svelte app!</p>
+  
+  <style>
+    h1 {
+      color: darkred;
+    }
+  </style>
+  
