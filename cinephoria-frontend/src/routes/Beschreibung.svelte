c28f9f0de113bb4fa2f3e1126ff463fe01197a2f
Christian Zweigert <christian.zweigert@krugundpartner.com>
2024-11-27 11:18:10 +0100

    Beschreibung geht local

--- cinephoria-frontend/src/routes/Beschreibung.svelte
+++ cinephoria-frontend/src/routes/Beschreibung.svelte
@@ -1,31 +1,37 @@
 <script>
-    import { onMount } from 'svelte';
-  
-    export let params;
-    const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
-    let movieId = params.id;
-    let movieDetails = {};
-  
-    onMount(async () => {
-      try {
-        const response = await fetch(
-          `https://cinephoria-backend-c53f94f0a255.herokuapp.com/movies/${movieId}`
-        );
+  export let id; // ID wird von der Route übergeben
+  console.log('Geladene ID:', id); // Debugging
+
+  import { onMount } from 'svelte';
+  let movieDetails = {};
+  const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+
+  onMount(async () => {
+    if (!id) {
+      console.error('Keine ID gefunden!');
+      return;
+    }
+    try {
+      const response = await fetch(
+        `https://cinephoria-backend-c53f94f0a255.herokuapp.com/movies/${id}`
+      );
+      if (response.ok) {
         movieDetails = await response.json();
-      } catch (error) {
-        console.error('Fehler beim Laden der Filmdetails:', error);
+      } else {
+        console.error('Fehler beim Laden der Filmdetails:', response.statusText);
       }
-    });
-  </script>
-  
-  {#if movieDetails.title}
-    <main>
-      <h1>{movieDetails.title}</h1>
-      <img src="{IMAGE_BASE_URL}{movieDetails.poster_path}" alt="{movieDetails.title}" />
-      <p>{movieDetails.overview}</p>
-      <!-- Weitere Details anzeigen -->
-    </main>
-  {:else}
-    <p>Lade Filmdetails...</p>
-  {/if}
-  
+    } catch (error) {
+      console.error('Netzwerkfehler:', error);
+    }
+  });
+</script>
+
+{#if movieDetails.title}
+  <main>
+    <h1>{movieDetails.title}</h1>
+    <img src="{IMAGE_BASE_URL}{movieDetails.poster_path}" alt="{movieDetails.title}" />
+    <p>{movieDetails.overview}</p>
+  </main>
+{:else}
+  <p>Lade Filmdetails...</p>
+{/if}
