e86221c8864c27c0fb680287f658a698a52236a2
Christian Zweigert <christian.zweigert@krugundpartner.com>
2024-11-27 10:06:28 +0100

    [id] hinzugefügt und ordner beschreibung

--- cinephoria-frontend/src/routes/Beschreibung.svelte
+++ cinephoria-frontend/src/routes/Beschreibung.svelte
@@ -1,13 +1,11 @@
 <script>
-    import { onMount, getContext } from 'svelte';
-
-    // Zugriff auf die Routenparameter
-    const { params } = getContext('svelte-routing'); // Hinzugefügt
+    import { onMount } from 'svelte';
+  
+    export let params;
     const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
     let movieId = params.id;
     let movieDetails = {};
-
-    // Beim Mounten der Komponente die Filmdetails abrufen
+  
     onMount(async () => {
       try {
         const response = await fetch(
@@ -18,15 +16,16 @@
         console.error('Fehler beim Laden der Filmdetails:', error);
       }
     });
-</script>
-
-{#if movieDetails.title}
-  <main>
-    <h1>{movieDetails.title}</h1>
-    <img src="{IMAGE_BASE_URL}{movieDetails.poster_path}" alt="{movieDetails.title}" />
-    <p>{movieDetails.overview}</p>
-    <!-- Weitere Details anzeigen -->
-  </main>
-{:else}
-  <p>Lade Filmdetails...</p>
-{/if}
+  </script>
+  
+  {#if movieDetails.title}
+    <main>
+      <h1>{movieDetails.title}</h1>
+      <img src="{IMAGE_BASE_URL}{movieDetails.poster_path}" alt="{movieDetails.title}" />
+      <p>{movieDetails.overview}</p>
+      <!-- Weitere Details anzeigen -->
+    </main>
+  {:else}
+    <p>Lade Filmdetails...</p>
+  {/if}
+  
---
+++ cinephoria-frontend/src/routes/Beschreibung/[id].svelte
@@ -0,0 +1,10 @@
+<script>
+    import { page } from '$app/stores';
+  
+    // Die ID aus den Parametern holen
+    $: id = $page.params.id;
+  </script>
+  
+  <h1>Hallo, das ist eine Beschreibung</h1>
+  <p>Die ID lautet: {id}</p>
+  
