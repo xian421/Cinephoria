861777d9e3a930f16df834a47bacecb7f0c7b41c
Christian Zweigert <christian.zweigert@krugundpartner.com>
2024-11-27 18:10:25 +0100

    first try sitzplan

---
+++ cinephoria-frontend/src/routes/Sitzplan.svelte
@@ -0,0 +1,43 @@
+<div class="seat-map">
+    <div class="row" data-row="A">
+      <div class="seat" data-seat="1"></div>
+      <div class="seat reserved" data-seat="2"></div>
+      <div class="seat" data-seat="3"></div>
+      <div class="seat" data-seat="4"></div>
+    </div>
+    <div class="row" data-row="B">
+      <div class="seat" data-seat="1"></div>
+      <div class="seat reserved" data-seat="2"></div>
+      <div class="seat" data-seat="3"></div>
+      <div class="seat" data-seat="4"></div>
+    </div>
+  </div>
+
+<style>
+  .seat-map {
+    display: grid;
+    gap: 10px;
+  }
+  
+  .row {
+    display: flex;
+    justify-content: center;
+  }
+  
+  .seat {
+    width: 30px;
+    height: 30px;
+    background-color: #4caf50;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+  
+  .seat.reserved {
+    background-color: #f44336;
+    cursor: not-allowed;
+  }
+  
+  .seat:hover:not(.reserved) {
+    background-color: #ffeb3b;
+  }
+</style>
