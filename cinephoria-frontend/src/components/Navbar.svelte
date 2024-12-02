f2a633bebf5a834e0fd57aa72cb689b477b4ce2b
nevi02 <s232729@student.dhbw-mannheim.de>
2024-12-02 12:51:13 +0100

    Update Navbar.svelte

--- cinephoria-frontend/src/components/Navbar.svelte
+++ cinephoria-frontend/src/components/Navbar.svelte
@@ -194,6 +194,7 @@
     .profile-dropdown-menu li:hover {
       background-color: #f0f0f0;
       border-radius: 8px;
+      border-color: #1abc9c;
       
     }
   
@@ -206,7 +207,7 @@
       font-weight: bold;
       color: black;
       transition: all 0.3s ease;
-      border-color: #1abc9c;
+  
     }
   
     .profile-container:hover {
