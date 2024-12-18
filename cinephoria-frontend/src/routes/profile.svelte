fa0cd78b6e2ae8e143d0572297c00f5b267a6eff
Christian Zweigert <christian.zweigert@krugundpartner.com>
2024-12-18 20:38:07 +0100

    Profile bearbeiten2

--- cinephoria-frontend/src/routes/profile.svelte
+++ cinephoria-frontend/src/routes/profile.svelte
@@ -8,7 +8,7 @@
     import { navigate } from 'svelte-routing';
     import ProfileImage from './ProfileImage.svelte';
     import ProfileModal from './ProfileModal.svelte';
-    import EditProfileModal from './EditProfileModal.svelte';
+    import EditProfileModal from './Editprofilemodal.svelte';
     
     let profile = {
         vorname: '',
