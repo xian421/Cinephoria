d7f2a934d2dec159609cc44e87dc79a43a1ab2e7
Christian Zweigert <christian.zweigert@krugundpartner.com>
2024-12-19 10:03:27 +0100

    Leaderboard Backend2

--- cinephoria-backend/app.py
+++ cinephoria-backend/app.py
@@ -2210,7 +2210,7 @@ def get_leaderboard():
         with psycopg2.connect(DATABASE_URL) as conn:
             with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
                 cursor.execute("""
-                    SELECT up.user_id, u.username, up.points, u.profile_image
+                    SELECT up.user_id, u.nickname, up.points, u.profile_image
                     FROM users u
                     JOIN user_points up ON u.id = up.user_id
                     ORDER BY up.points DESC
