d43bc2bcaa3c3de66053ff96087765390abb7ab6
Christian Zweigert <christian.zweigert@krugundpartner.com>
2024-12-19 11:22:42 +0100

    Leaderboard Backend4

--- cinephoria-backend/app.py
+++ cinephoria-backend/app.py
@@ -2210,7 +2210,7 @@ def get_leaderboard():
         with psycopg2.connect(DATABASE_URL) as conn:
             with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
                 cursor.execute("""
-                    SELECT up.user_id, u.nickname, up.points, u.profile_image, COUNT(b.booking_id) AS bookings, MAX(s.start_time) AS last_booking, SUM(s.end_time - s.start_time) AS total_duration
+                    SELECT up.user_id, u.nickname, up.points, u.profile_image, COUNT(b.booking_id) AS bookings, MAX(s.start_time) AS last_booking, SUM(EXTRACT(EPOCH FROM (s.end_time - s.start_time)) / 60) AS total_duration
                     FROM users u
                     JOIN user_points up ON u.id = up.user_id
                     LEFT JOIN bookings b ON u.id = b.user_id
