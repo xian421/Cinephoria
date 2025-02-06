053b01899db696b9267322df4b05b38c0ee48e6b
Christian Zweigert <christian.zweigert@krugundpartner.com>
2025-02-06 11:09:58 +0100

    Secret KEy Fehler2

--- cinephoria_backend/tests/test_example.py
+++ cinephoria_backend/tests/test_example.py
@@ -1,3 +1,6 @@
+import os
+os.environ["SECRET_KEY"] = "testsecret"
+
 from cinephoria_backend.routes import paypal
 from cinephoria_backend.config import PAYPAL_API_BASE, PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET, get_db_connection, SECRET_KEY, TMDB_API_URL, HEADERS, DATABASE_URL, TMDB_BEARER_TOKEN
 
